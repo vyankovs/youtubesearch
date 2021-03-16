d392f27909ff5c91cfc92f6f461bbad10975163e
vyankovs <viktoriia.yankovska@gmail.com>
2021-03-16 18:09:57 +0100

    first commit

---
+++ src/components/VideoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="list-group col-4">
+    <VideoListItem 
+    v-for="video in videos" 
+    :key="video.etag" 
+    :video ="video"
+    @videoSelect="onVideoSelect">
+    </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem'
+
+export default {
+   name: 'VideoList',
+   components: {
+       VideoListItem
+   },
+   props: ['videos'],
+   methods: {
+       onVideoSelect(video){
+           this.$emit('videoSelect', video)
+       }
+   }
+
+   }
+
+</script>
+
+<style >
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+new Vue ({
+    
+    render: h => h(App)
+}).$mount('#app');
